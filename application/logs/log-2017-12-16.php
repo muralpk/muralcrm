172d260a94292b823bd47bb740c99dd3af434e6c
muralpakistan <muralpakistan@gmail.com>
2017-12-22 20:41:14 +0500

    pata nai

---
+++ application/logs/log-2017-12-16.php
@@ -0,0 +1,73 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-12-16 23:23:43 --> Severity: Notice --> Undefined variable: google_ids_calendars C:\xampp\htdocs\perfex\application\views\admin\directory\home.php 32
+ERROR - 2017-12-16 23:23:43 --> Severity: Notice --> Undefined variable: tickets_awaiting_reply_by_department C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 14
+ERROR - 2017-12-16 23:23:43 --> Severity: Notice --> Undefined variable: tickets_reply_by_status C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 21
+ERROR - 2017-12-16 23:23:43 --> Severity: Notice --> Undefined variable: leads_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 28
+ERROR - 2017-12-16 23:23:43 --> Severity: Notice --> Undefined variable: projects_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 36
+ERROR - 2017-12-16 23:23:43 --> Severity: Notice --> Undefined variable: weekly_payment_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 43
+ERROR - 2017-12-16 23:24:57 --> Severity: Notice --> Undefined variable: google_ids_calendars C:\xampp\htdocs\perfex\application\views\admin\directory\home.php 32
+ERROR - 2017-12-16 23:24:57 --> Severity: Notice --> Undefined variable: tickets_awaiting_reply_by_department C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 14
+ERROR - 2017-12-16 23:24:57 --> Severity: Notice --> Undefined variable: tickets_reply_by_status C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 21
+ERROR - 2017-12-16 23:24:57 --> Severity: Notice --> Undefined variable: leads_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 28
+ERROR - 2017-12-16 23:24:57 --> Severity: Notice --> Undefined variable: projects_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 36
+ERROR - 2017-12-16 23:24:57 --> Severity: Notice --> Undefined variable: weekly_payment_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 43
+ERROR - 2017-12-16 23:25:10 --> Severity: Notice --> Undefined variable: google_ids_calendars C:\xampp\htdocs\perfex\application\views\admin\directory\call.php 56
+ERROR - 2017-12-16 23:25:10 --> Severity: Notice --> Undefined variable: tickets_awaiting_reply_by_department C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 14
+ERROR - 2017-12-16 23:25:10 --> Severity: Notice --> Undefined variable: tickets_reply_by_status C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 21
+ERROR - 2017-12-16 23:25:10 --> Severity: Notice --> Undefined variable: leads_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 28
+ERROR - 2017-12-16 23:25:10 --> Severity: Notice --> Undefined variable: projects_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 36
+ERROR - 2017-12-16 23:25:10 --> Severity: Notice --> Undefined variable: weekly_payment_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 43
+ERROR - 2017-12-16 23:25:39 --> Severity: Notice --> Undefined variable: google_ids_calendars C:\xampp\htdocs\perfex\application\views\admin\directory\call.php 56
+ERROR - 2017-12-16 23:25:39 --> Severity: Notice --> Undefined variable: tickets_awaiting_reply_by_department C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 14
+ERROR - 2017-12-16 23:25:39 --> Severity: Notice --> Undefined variable: tickets_reply_by_status C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 21
+ERROR - 2017-12-16 23:25:39 --> Severity: Notice --> Undefined variable: leads_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 28
+ERROR - 2017-12-16 23:25:39 --> Severity: Notice --> Undefined variable: projects_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 36
+ERROR - 2017-12-16 23:25:39 --> Severity: Notice --> Undefined variable: weekly_payment_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 43
+ERROR - 2017-12-16 23:25:52 --> Severity: Notice --> Undefined variable: google_ids_calendars C:\xampp\htdocs\perfex\application\views\admin\directory\call.php 56
+ERROR - 2017-12-16 23:25:52 --> Severity: Notice --> Undefined variable: tickets_awaiting_reply_by_department C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 14
+ERROR - 2017-12-16 23:25:52 --> Severity: Notice --> Undefined variable: tickets_reply_by_status C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 21
+ERROR - 2017-12-16 23:25:52 --> Severity: Notice --> Undefined variable: leads_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 28
+ERROR - 2017-12-16 23:25:52 --> Severity: Notice --> Undefined variable: projects_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 36
+ERROR - 2017-12-16 23:25:52 --> Severity: Notice --> Undefined variable: weekly_payment_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 43
+ERROR - 2017-12-16 23:27:05 --> Severity: Notice --> Undefined variable: google_ids_calendars C:\xampp\htdocs\perfex\application\views\admin\directory\home.php 32
+ERROR - 2017-12-16 23:27:05 --> Severity: Notice --> Undefined variable: tickets_awaiting_reply_by_department C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 14
+ERROR - 2017-12-16 23:27:05 --> Severity: Notice --> Undefined variable: tickets_reply_by_status C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 21
+ERROR - 2017-12-16 23:27:05 --> Severity: Notice --> Undefined variable: leads_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 28
+ERROR - 2017-12-16 23:27:05 --> Severity: Notice --> Undefined variable: projects_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 36
+ERROR - 2017-12-16 23:27:05 --> Severity: Notice --> Undefined variable: weekly_payment_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 43
+ERROR - 2017-12-16 23:32:37 --> Severity: Notice --> Undefined variable: google_ids_calendars C:\xampp\htdocs\perfex\application\views\admin\directory\call.php 56
+ERROR - 2017-12-16 23:32:37 --> Severity: Notice --> Undefined variable: tickets_awaiting_reply_by_department C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 14
+ERROR - 2017-12-16 23:32:37 --> Severity: Notice --> Undefined variable: tickets_reply_by_status C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 21
+ERROR - 2017-12-16 23:32:37 --> Severity: Notice --> Undefined variable: leads_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 28
+ERROR - 2017-12-16 23:32:37 --> Severity: Notice --> Undefined variable: projects_status_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 36
+ERROR - 2017-12-16 23:32:37 --> Severity: Notice --> Undefined variable: weekly_payment_stats C:\xampp\htdocs\perfex\application\views\admin\includes\dashboard_js.php 43
+ERROR - 2017-12-16 20:00:27 --> Severity: error --> Exception: syntax error, unexpected '"Error loading page: Error Num' (T_CONSTANT_ENCAPSED_STRING) C:\xampp\htdocs\perfex\application\controllers\admin\The_directory.php 50
+ERROR - 2017-12-16 20:15:53 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:16:20 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:17:21 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:19:56 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:22:20 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:27:37 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:29:03 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:29:20 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:30:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:31:00 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:32:25 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:33:30 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:37:19 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:41:22 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:43:51 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:44:36 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:47:04 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:48:51 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:52:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:55:40 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:58:10 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 20:59:39 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 21:00:09 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 21:01:39 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 21:05:39 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 21:06:04 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 21:06:58 --> 404 Page Not Found: Assets/plugins
+ERROR - 2017-12-16 21:08:57 --> 404 Page Not Found: Assets/plugins
---
+++ application/logs/log-2017-12-18.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> date_format() expects parameter 1 to be DateTimeInterface, boolean given C:\xampp\htdocs\perfex\application\helpers\perfex_general_helper.php 401
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> date_format() expects parameter 1 to be DateTimeInterface, boolean given C:\xampp\htdocs\perfex\application\helpers\perfex_general_helper.php 401
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> date_format() expects parameter 1 to be DateTimeInterface, boolean given C:\xampp\htdocs\perfex\application\helpers\perfex_general_helper.php 401
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> date_format() expects parameter 1 to be DateTimeInterface, boolean given C:\xampp\htdocs\perfex\application\helpers\perfex_general_helper.php 401
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> date_format() expects parameter 1 to be DateTimeInterface, boolean given C:\xampp\htdocs\perfex\application\helpers\perfex_general_helper.php 401
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> date_format() expects parameter 1 to be DateTimeInterface, boolean given C:\xampp\htdocs\perfex\application\helpers\perfex_general_helper.php 401
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> date_format() expects parameter 1 to be DateTimeInterface, boolean given C:\xampp\htdocs\perfex\application\helpers\perfex_general_helper.php 401
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> date_format() expects parameter 1 to be DateTimeInterface, boolean given C:\xampp\htdocs\perfex\application\helpers\perfex_general_helper.php 401
+ERROR - 2017-12-18 21:57:06 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp\htdocs\perfex\system\core\Exceptions.php:271) C:\xampp\htdocs\perfex\system\helpers\url_helper.php 564
+ERROR - 2017-12-18 18:04:30 --> Severity: error --> Exception: syntax error, unexpected ';', expecting ',' or ')' C:\xampp\htdocs\perfex\application\controllers\admin\Contracts.php 362
+ERROR - 2017-12-18 18:06:40 --> Severity: error --> Exception: syntax error, unexpected ';', expecting ',' or ')' C:\xampp\htdocs\perfex\application\controllers\admin\Contracts.php 362
+ERROR - 2017-12-18 18:06:45 --> Severity: error --> Exception: syntax error, unexpected ';', expecting ',' or ')' C:\xampp\htdocs\perfex\application\controllers\admin\Contracts.php 362
+ERROR - 2017-12-18 18:09:58 --> Severity: Compile Error --> Constant expression contains invalid operations C:\xampp\htdocs\perfex\application\controllers\admin\Contracts.php 360
+ERROR - 2017-12-18 18:09:59 --> Severity: Compile Error --> Constant expression contains invalid operations C:\xampp\htdocs\perfex\application\controllers\admin\Contracts.php 360
+ERROR - 2017-12-18 18:10:02 --> Severity: Compile Error --> Constant expression contains invalid operations C:\xampp\htdocs\perfex\application\controllers\admin\Contracts.php 360
